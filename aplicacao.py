fba0eb766058b9c57383c46773673d147f6fec25
GabrielMendes <gabrieldesamendes@gmail.com>
2025-11-04 20:32:56 -0300

    Update aplicacao.py

--- aplicacao.py
+++ aplicacao.py
@@ -1,4 +1,4 @@
-from typing import Protocol, Optional 
+from typing import Protocol, Optional
 
 
 class CoffeMachineProtocol(Protocol):
@@ -8,5 +8,55 @@ class CoffeMachineProtocol(Protocol):
 
 
 class CoffeeApp:
-    def __ini__(self, delegate: CoffeMachineProtocol):
-        self.delegate = delegate     
+    # erro: era __ini__, o correto é __init__
+    def __init__(self, delegate: CoffeMachineProtocol):
+        self.delegate = delegate
+
+    # precisa ter 'self' como primeiro parâmetro
+    def order(self, coffeNome: str):
+        # usar self.delegate (não apenas delegate)
+        self.delegate.start()
+        self.delegate.create(coffeNome=coffeNome)
+        self.delegate.end()
+
+
+class JoaquinMachineCoffe:
+    def start(self):
+        print("Joaquin's Coffee starting.......vrrrruuuuuuuuuuuu")
+
+    def create(self, coffeNome: str):
+        print("Verificando o café solicitado")
+
+        # operador correto: 'or', não '|'
+        if coffeNome == "espresso" or coffeNome == "capuccino":
+            print("Café saindo, aguarde um pouco!")
+        else:
+            print("Café não encontrado - ERROU HAHA")
+
+    def end(self):
+        print("Power off")
+
+
+class CachorroMachineCoffe:
+    def start(self):
+        print("Cachorro's Coffee AUAUAUUAUAUAUAUAUUAUAUAU starting.......vrrrruuuuuuuuuuuu")
+
+    def create(self, coffeNome: str):
+        print("Verificando o café solicitado")
+
+        # operador correto: 'or', não '|'
+        if coffeNome == "Pingado":
+            print("Café saindo, aguarde um pouco!")
+        else:
+            print("Café não encontrado - ERROU HAHA")
+
+    def end(self):
+        print("Power off")
+
+
+
+# Nome da Classe e Instância
+cachorroMachine = CachorroMachineCoffe()
+joaquinMachine = JoaquinMachineCoffe()
+app = CoffeeApp(delegate = cachorroMachine)
+app.order("pingado") 
